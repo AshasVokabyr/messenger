c8e603a3bc928c44b87a04f152ee93a1aa77870d
DrMaxwellMaello <maello.fox@gmail.com>
2026-07-12 17:18:49 +0300

    Revert "Issue 6"
    This reverts commit 1de89a1c4a178a40e577d03d6c3aa0d54bf7aac0.

--- app/models/message.py
+++ app/models/message.py
@@ -29,5 +29,4 @@ class Message(Base):
 
     __table_args__ = (
         Index("ix_messages_chat_created", "chat_id", created_at.desc()),
-        Index("ix_messages_chat_content", "chat_id", "content"),
     )
